082ed47d1d2ad07f752b56cd6e7ab1718ac24ec0
rigo_sarmiento <rigorei.sarmiento@gmail.com>
2019-01-10 15:33:16 +0800

    Indentations

--- src/main/java/rigor/io/paragala/voter/voting/VotingController.java
+++ src/main/java/rigor/io/paragala/voter/voting/VotingController.java
@@ -17,70 +17,71 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class VotingController {
 
-    private Admin admin;
-    private VoteBox voteBox;
+  private Admin admin;
+  private VoteBox voteBox;
 
-    public VotingController(Admin admin, VoteBox voteBox) {
-        this.admin = admin;
-        this.voteBox = voteBox;
+  public VotingController(Admin admin, VoteBox voteBox) {
+    this.admin = admin;
+    this.voteBox = voteBox;
+  }
+
+  @PostMapping("/vote")
+  public ResponseEntity<?> vote(@RequestBody Map<String, Object> vote) throws IOException {
+    Optional<Voter> voter = admin.validateVoter(
+        String.valueOf(vote.get("id")),
+        String.valueOf(vote.get("code")),
+        String.valueOf(vote.get("school"))
+    );
+    System.out.println(vote);
+    vote.put("voter", voter);
+    return voter.isPresent()
+        ? canVote(vote)
+        ? registerVote(vote)
+        : new ResponseEntity<>(getError("401", "Voter no longer allowed to vote"), HttpStatus.UNAUTHORIZED)
+        : new ResponseEntity<>(getError("401", "Voter not found"), HttpStatus.UNAUTHORIZED);
+  }
+
+  //will require token
+  @GetMapping("/votes")
+  public ResponseEntity<?> viewVotes() {
+    return new ResponseEntity<>(voteBox.viewVotes(), HttpStatus.OK);
+  }
+
+  private Map<String, String> getError(String status, String message) {
+    Map<String, String> map = new HashMap<>();
+    map.put("status", status);
+    map.put("message", message);
+    return map;
+  }
+
+  private boolean canVote(Map<String, Object> vote) {
+    Optional<Voter> voter = (Optional) vote.get("voter");
+    boolean b = voter.isPresent();
+    if (b) {
+      System.out.println("hatdog?");
+      vote.put("name", voter.get().getName());
+      // change voter status to false n
     }
+    return b;
+  }
 
-    @PostMapping("/vote")
-    public ResponseEntity<?> vote(@RequestBody Map<String, Object> vote) throws IOException {
-        Optional<Voter> voter = admin.validateVoter(
-                String.valueOf(vote.get("id")),
-                String.valueOf(vote.get("code")),
-                String.valueOf(vote.get("school"))
-        );
-        System.out.println(vote);
-        vote.put("voter", voter);
-        return voter.isPresent()
-                ? canVote(vote)
-                ? registerVote(vote)
-                : new ResponseEntity<>(getError("401", "Voter no longer allowed to vote"), HttpStatus.UNAUTHORIZED)
-                : new ResponseEntity<>(getError("401", "Voter not found"), HttpStatus.UNAUTHORIZED);
-    }
+  private ResponseEntity<?> registerVote(Map<String, Object> vote) throws IOException {
+    System.out.println(vote);
 
-    public Map<String, String> getError(String status, String message) {
-        Map<String, String> map = new HashMap<>();
-        map.put("status", status);
-        map.put("message", message);
-        return map;
-    }
-
-    private boolean canVote(Map<String, Object> vote) {
-        Optional<Voter> voter = (Optional) vote.get("voter");
-        boolean b = voter.isPresent();
-        if (b) {
-            System.out.println("hatdog?");
-        vote.put("name", voter.get().getName());
-            // change voter status to false n
-        }
-        return b;
-    }
-
-    @GetMapping("")
-    public ResponseEntity<?> viewVotes() {
-        return new ResponseEntity<>(voteBox.viewVotes(), HttpStatus.OK);
-    }
-
-    private ResponseEntity<?> registerVote(Map<String, Object> vote) throws IOException {
-        System.out.println(vote);
-
-        VoteForm voteForm = new VoteForm(
-                String.valueOf(vote.get("name")),
-                String.valueOf(vote.get("id")),
-                String.valueOf(vote.get("code")),
-                String.valueOf(vote.get("school")));
-        List<Nominee> nominees = new ObjectMapper().readValue(new ObjectMapper().writeValueAsString(vote.get("votes")),
-                new TypeReference<List<Nominee>>(){});
-        nominees.forEach(System.out::println);
-        voteForm.setNominees(nominees);
-        voteBox.vote(voteForm);
-        Map<String, String> response = new HashMap<>();
-        response.put("status", "200");
-        response.put("message", "Vote Confirmed");
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
+    VoteForm voteForm = new VoteForm(
+        String.valueOf(vote.get("name")),
+        String.valueOf(vote.get("id")),
+        String.valueOf(vote.get("code")),
+        String.valueOf(vote.get("school")));
+    List<Nominee> nominees = new ObjectMapper().readValue(new ObjectMapper().writeValueAsString(vote.get("votes")),
+                                                          new TypeReference<List<Nominee>>() {});
+    nominees.forEach(System.out::println);
+    voteForm.setNominees(nominees);
+    voteBox.vote(voteForm);
+    Map<String, String> response = new HashMap<>();
+    response.put("status", "200");
+    response.put("message", "Vote Confirmed");
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
 
 }
