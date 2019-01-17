e5385ecb248bc9022f4f1f4684fb6d2ae54279b7
rigo_sarmiento <rigorei.sarmiento@gmail.com>
2019-01-17 09:04:28 +0800

    Completed admin user privileges

--- src/main/java/rigor/io/paragala/voter/user/UserController.java
+++ src/main/java/rigor/io/paragala/voter/user/UserController.java
@@ -3,6 +3,7 @@ package rigor.io.paragala.voter.user;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import rigor.io.paragala.voter.ResponseHub;
 import rigor.io.paragala.voter.token.TokenService;
 
 import java.util.Map;
@@ -10,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/api")
 public class UserController {
 
   private TokenService tokenService;
@@ -18,8 +20,7 @@ public class UserController {
   public UserController(TokenService tokenService, UserRepository userRepository) {
     this.tokenService = tokenService;
     this.userRepository = userRepository;
-    User defaultUser = this.userRepository.save(new User("paragala.ph", "p4r4g4l4"));
-    System.out.println(defaultUser);
+    this.userRepository.save(new User("paragala.ph", "p4r4g4l4"));
   }
 
   @PostMapping("/user")
@@ -27,9 +28,42 @@ public class UserController {
     userRepository.save(new User(username, password));
   }
 
+  @DeleteMapping("/users/{user}")
+  public ResponseEntity<?> deleteUser(@RequestParam(required = false) String token,
+                                      @PathVariable String user) {
+    if (!tokenService.isValid(token))
+      return ResponseHub.defaultUnauthorizedResponse();
+    userRepository.deleteByUsername(user);
+    return new ResponseEntity<>("deleted", HttpStatus.ACCEPTED);
+  }
+
+  @PostMapping("/users")
+  public ResponseEntity<?> create(@RequestParam(required = false) String token,
+                                  @RequestBody Map<String, String> data) {
+
+    if (!tokenService.isValid(token))
+      return ResponseHub.defaultUnauthorizedResponse();
+
+    String username = tokenService.fetchUser(token).getUsername();
+    String password = data.get("currentPassword");
+
+    Optional<User> user = userRepository.findByUsernameAndPassword(username, password);
+
+    if (!user.isPresent())
+      return ResponseHub.defaultUnauthorizedResponse(); // change to "wrong password"
+    if (userRepository.findAll().size() > 4)
+      return new ResponseEntity<>("2 much", HttpStatus.BAD_REQUEST);
+
+    String newUsername = data.get("username");
+    if (userRepository.findByUsername(newUsername).isPresent())
+      return new ResponseEntity<>("no wae", HttpStatus.BAD_REQUEST);
+    String newPassword = data.get("password");
+    return new ResponseEntity<>(userRepository.save(new User(newUsername, newPassword)), HttpStatus.OK);
+  }
+
   @PostMapping("/login")
   public ResponseEntity<?> login(@RequestBody Map<String, String> credentials) {
-    if (credentials.get("username")==null && credentials.get("password")==null)
+    if (credentials.get("username") == null && credentials.get("password") == null)
       return new ResponseEntity<>("is no good papi", HttpStatus.BAD_REQUEST);
 
     String username = credentials.get("username");
@@ -41,7 +75,7 @@ public class UserController {
   }
 
   @PostMapping("/logout")
-  public ResponseEntity<?> logout(String token) {
+  public ResponseEntity<?> logout(@RequestParam(required = false) String token) {
     return tokenService.isValid(token)
         ? new ResponseEntity<>(logoutUser(token), HttpStatus.OK)
         : new ResponseEntity<>(" alrady logged out", HttpStatus.NOT_FOUND);
--- src/main/java/rigor/io/paragala/voter/user/UserRepository.java
+++ src/main/java/rigor/io/paragala/voter/user/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User, Long> {
   List<User> findAll();
   Optional<User> findByUsernameAndPassword(String username, String password);
+  Optional<User> findByUsername(String username);
+  void deleteByUsername(String username);
 }
