ea4fa4b1020521c5da71138b4c72d302aaf77908
rigorio <rigo.sarmiento@pm..me>
2019-01-18 23:26:21 +0800

    Added transactional

--- src/main/java/rigor/io/paragala/voter/details/SchoolRepository.java
+++ src/main/java/rigor/io/paragala/voter/details/SchoolRepository.java
@@ -2,9 +2,11 @@ package rigor.io.paragala.voter.details;
 
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface SchoolRepository extends CrudRepository<School, Long> {
   List<School> findAll();
+  @Transactional
   void deleteByName(String name);
 }
