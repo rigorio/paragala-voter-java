725b6afe911de781c13029659d58a47324d6b355
rigorio <rigo.sarmiento@pm..me>
2019-01-18 23:26:13 +0800

    Added transactional

--- src/main/java/rigor/io/paragala/voter/details/CategoryRepository.java
+++ src/main/java/rigor/io/paragala/voter/details/CategoryRepository.java
@@ -2,9 +2,11 @@ package rigor.io.paragala.voter.details;
 
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
   List<Category> findAll();
+  @Transactional
   void deleteByKategory(String kategory);
 }
